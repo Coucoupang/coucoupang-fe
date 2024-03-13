b45f4cd21966d1d949a0611e8272b059ec4411e4
kimdw524 <38282677+kimdw524@users.noreply.github.com>
2024-03-13 17:46:18 +0900

    feat: Button Component 추가

---
+++ src/components/atomics/Button.tsx
@@ -0,0 +1,50 @@
+/** @jsxImportSource @emotion/react */
+import { ColorPalette, Theme, css } from '@emotion/react';
+import React, { ReactNode } from 'react';
+
+interface ButtonProps extends React.ComponentProps<'button'> {
+  variant: keyof Theme['colors'];
+  children?: ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+  return (
+    <button css={(theme) => buttonStyle(theme, theme.colors[props.variant])} {...props}>
+      {props.children}
+    </button>
+  );
+};
+
+const buttonStyle = (theme: Theme, color: ColorPalette) => css`
+  position: relative;
+  padding: 0.625rem 1.125rem;
+  border: 0;
+  border-radius: 0.125rem;
+  background-color: ${color.background?.normal};
+  color: ${color.text?.normal};
+  transition: all 100ms ease;
+
+  :hover {
+    ::after {
+      opacity: 1;
+    }
+  }
+
+  :active {
+    background-color: ${color.background?.active};
+  }
+
+  ::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    box-shadow: 0 0 6px ${theme.boxShadow};
+    opacity: 0;
+    transition: opacity 150ms ease;
+    content: '';
+  }
+`;
+
+export default Button;
