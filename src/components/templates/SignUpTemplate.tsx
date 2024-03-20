a92d25958443a0793b3f596be59615e4fb69841d
kimdw524 <kimdw524@naver.com>
2024-03-21 01:21:44 +0900

    feat: SignUp Page Template

--- src/components/templates/SignUpTemplate.tsx
+++ src/components/templates/SignUpTemplate.tsx
@@ -4,16 +4,34 @@ import Card from '../molecules/Card';
 import { Theme, css } from '@emotion/react';
 import Text from '../atomics/Text';
 import Button from '../atomics/Button';
+import ButtonGroup from '../molecules/ButtonGroup';
 
 const SingUpTemplate = () => {
   return (
     <div css={(theme) => signUpContainer(theme)}>
-      <Text variant="primary" size="xl" weight="lg">
+      <Text
+        variant="primary"
+        size="xl"
+        weight="lg"
+        style={{ margin: '1.5rem 0', letterSpacing: '1px' }}
+      >
         회원가입
       </Text>
-      <div></div>
-      <Button variant="primary">일반 회원가입</Button>
-      <Button variant="secondary">판매 회원가입</Button>
+      <ButtonGroup separatorColor="#ddd" vertical>
+        <Button variant="secondary" style={{ padding: '1.25rem 0' }}>
+          휴대폰 번호로 가입하기
+        </Button>
+        <Button variant="secondary" style={{ padding: '1.25rem 0' }}>
+          카카오로 가입하기
+        </Button>
+        <Button variant="secondary" style={{ padding: '1.25rem 0' }}>
+          네이버로 가입하기
+        </Button>
+      </ButtonGroup>
+      <div css={sellerContainer}>
+        <Text size="md">쿠쿠팡에서 판매를 원하세요?</Text>
+        <Button variant="primary">판매자 가입하기</Button>
+      </div>
     </div>
   );
 };
@@ -22,6 +40,13 @@ const signUpContainer = (theme: Theme) => css`
   max-width: ${theme.breakPoints.sm};
   margin: 0 auto;
   padding: 1rem;
+`;
+
+const sellerContainer = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1.5rem 0;
 `;
 
 export default SingUpTemplate;
