08c3f7463cf0552d27ab1c7aabd05ead80414e7f
kimdw524 <kimdw524@naver.com>
2024-04-01 01:13:01 +0900

    임시 저장

--- src/components/atomics/TransitionAnimation.tsx
+++ src/components/atomics/TransitionAnimation.tsx
@@ -6,42 +6,62 @@ interface TransitionAnimation {
   children: ReactElement[];
 }
 
-type TransitionStatus = 'VISIBLE' | 'HIDDEN';
+type TransitionStatus = 'VISIBLE' | 'HIDDEN' | 'FADEOUT' | 'FADEIN';
 
 interface TransitionItem {
   status: TransitionStatus;
   item: ReactElement;
+  clone: ReactElement | null;
 }
 
 const TransitionAnimation = (props: TransitionAnimation) => {
-  const [items, setItems] = useState<TransitionItem[]>([]);
+  const childrenRef = useRef<TransitionItem[]>([]);
+  const [items, setItems] = useState<ReactElement[]>();
+
+  console.log(childrenRef.current);
 
   useEffect(() => {
-    setItems(
-      props.children.map((child) => {
-        return {
-          status: child.key === props['data-key'] ? 'VISIBLE' : 'HIDDEN',
-          item: child,
-        };
-      }),
+    childrenRef.current = props.children.map((child) => {
+      return {
+        status: child.key === props['data-key'] ? 'VISIBLE' : 'HIDDEN',
+        item: child,
+        clone: null,
+      };
+    });
+  }, []);
+
+  useEffect(() => {
+    childrenRef.current.some((child) => {
+      if (child.item.key === props['data-key']) {
+        if (child.status !== 'VISIBLE') child.status = 'FADEIN';
+        return true;
+      }
+      return false;
+    });
+
+    setItems(() =>
+      childrenRef.current.filter((i) => i.status !== 'HIDDEN').map((i) => i.clone || i.item),
     );
 
-    return () => {};
-  }, [props.children, props['data-key']]);
+    return () => {
+      childrenRef.current.some((child) => {
+        if (child.item.key === props['data-key']) {
+          if (child.status !== 'HIDDEN') {
+            child.status = 'FADEOUT';
+            child.clone = React.cloneElement(child.item, {
+              ref: (ref: HTMLDivElement) => {
+                console.log(ref);
+              },
+            });
+          }
+          return true;
+        }
+        return false;
+      });
+    };
+  }, [props['data-key']]);
 
-  /*  useEffect(() => {
-    console.log(props['data-key']);
-  }, [props['data-key']]);*/
-
-  return (
-    <>
-      {items
-        .filter((item) => {
-          return item.status === 'VISIBLE';
-        })
-        .map((child) => child.item)}
-    </>
-  );
+  return <>{items}</>;
 };
 
 export default TransitionAnimation;
